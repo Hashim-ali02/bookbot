6f44174ab55a409273f3cb66955ab013cf6129f1
Hashim-ali02 <hashim.ali2026@gmail.com>
2025-06-22 20:23:48 +0000

    Added system arguments and system exit upon incorrect usage

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_characters, sort_character_dict
 
 def get_book_text(filepath):
@@ -5,12 +6,15 @@ def get_book_text(filepath):
         return book.read()
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    text = get_book_text(sys.argv[1])
     num_words = count_words(text)
     count = count_characters(text)
     sorted_list = sort_character_dict(count)
     print("============ BOOKBOT ============") 
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
